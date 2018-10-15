5be274d794961a6c52eb9bc9f5b5da85b025805b
Shivanibusireddy <S531494@nwmissouri.edu>
2018-10-15 17:09:35 -0500

    Added Different view of Fetch Execute cycle

---
+++ IOS Project/FetchExecuteViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FetchExecuteViewController.swift
+//  IOS Project
+//
+//  Created by student on 10/15/18.
+//  Copyright © 2018 vijay kumar. All rights reserved.
+//
+
+import UIKit
+
+class FetchExecuteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
