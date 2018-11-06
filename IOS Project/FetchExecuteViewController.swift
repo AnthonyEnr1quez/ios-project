750d97ab41fd391860890d78e124ae99cc752459
Shivanibusireddy <S531494@nwmissouri.edu>
2018-11-05 18:08:40 -0600

    Added fully functional code of fetch Execute cycle with the delay

--- IOS Project/FetchExecuteViewController.swift
+++ IOS Project/FetchExecuteViewController.swift
@@ -81,11 +81,122 @@ class FetchExecuteViewController: UIViewController{
         InsAddressTXT.text = "\(800)"
         addMemTXT.text = "\(800)"
         ContentMemTXT.text = "ADD 4000, 2000, 2080"
-        UIView.animate(withDuration: 1.0, delay: 3.5, options: [], animations: {self.fetch()}, completion: nil)
-   UIView.animate(withDuration: 1.0, delay: 10, options: [], animations: {self.decode()}, completion: nil)
-     UIView.animate(withDuration: 1.0, delay: 15.5, options: [], animations: {self.dataFetch()}, completion: nil)
-        UIView.animate(withDuration: 1.0, delay: 17.5, options: [], animations: {self.iExecution()}, completion: nil)
- UIView.animate(withDuration: 1.0, delay: 20.5, options: [], animations: {self.rresult()}, completion: nil)
+        
+       
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
+            
+            self.FetchTXT.backgroundColor = UIColor.orange
+            
+            self.Program_counterTXT.text = "\(800)"
+            
+            self.InstructionTXT.text = "ADD 4000, 2000, 2080"
+            
+            }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 20.0) {
+            
+            self.decodeTXT.backgroundColor = UIColor.orange
+            self.FetchTXT.backgroundColor = UIColor.white
+            self.InsAddressTXT.isHidden = true
+            self.ReturnADDTXT.text = "4000"
+            self.data1MemTXT.text = "2000"
+            self.data2MemTXT.text = "2080"
+            
+            }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 30.0) {
+            
+            self.DFetchTXT.backgroundColor = UIColor.orange
+            self.decodeTXT.backgroundColor = UIColor.white
+            self.FetchTXT.backgroundColor = UIColor.white
+            self.InsAddressTXT.isHidden = true
+            self.data1TXT.text = "\(30)"
+            self.data2TXT.text = "\(12)"
+            self.DataAdd1TXT.text = "\(2000)"
+            self.DataAdd2TXT.text = "\(2080)"
+            self.ResultAddTXT.text = "\(4000)"
+            self.DataMEM1TXT.text = "\(30)"
+            self.DataMEM2TXT.text = "\(12)"
+            
+            
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 40.0) {
+            
+            self.ExecuteTXT.backgroundColor = UIColor.orange
+            self.DFetchTXT.backgroundColor = UIColor.white
+            self.decodeTXT.backgroundColor = UIColor.white
+            self.FetchTXT.backgroundColor = UIColor.white
+            self.InstructionTXT.isHidden = true
+            self.InsAddressTXT.isHidden = true
+            self.resultTXT.text = "\(42)"
+            self.data1MemTXT.isHidden = true
+            self.data2MemTXT.isHidden = true
+            
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 60.0) {
+            
+            self.rreturn.backgroundColor = UIColor.orange
+            self.ExecuteTXT.backgroundColor = UIColor.white
+            self.DFetchTXT.backgroundColor = UIColor.white
+            self.decodeTXT.backgroundColor = UIColor.white
+            self.FetchTXT.backgroundColor = UIColor.white
+            self.InstructionTXT.isHidden = true
+            
+            self.data1TXT.isHidden = true
+            self.data2TXT.isHidden = true
+            self.InsAddressTXT.text = "\(800)"
+            self.ResultMEMTXT.text = "\(42)"
+            
+        }
+//        delay(4.0){
+//            fetch()
+//
+//        }
+//        func delay(delay: Double, closure: ()->()) {
+//
+//
+//
+//            dispatch_after(
+//                dispatch_time(
+//                    dispatch_time_t(DISPATCH_TIME_NOW),
+//                    Int64(delay * Double(NSEC_PER_SEC))
+//                ),
+//                dispatch_get_main_queue(),
+//                closure
+//            )
+//        }
+//       let now = Date()
+//
+//     let now1 = now.addingTimeInterval(0.5*60.0)
+//       let now2 = now.addingTimeInterval(1*60.0)
+//         let now3 = now.addingTimeInterval(1.5*60.0)
+//         let now4 = now.addingTimeInterval(2*60.0)
+//         let now5 = now.addingTimeInterval(2.5*60.0)
+//
+//        if  now > now1
+//        {
+//            fetch()
+//           print("hai")
+//        }
+//        if  now1 > now2 {
+//            decode()
+//        }
+//        if now2 > now3
+//        {
+//            dataFetch()
+//        }
+//        if now3 > now4
+//        {
+//            iExecution()
+//        }
+//        if  now4 > now5
+//        {
+//            rresult()
+//        }
+//        UIView.animate(withDuration: 1.0, delay: 3.5, options: [], animations: {self.fetch()}, completion: nil)
+//   UIView.animate(withDuration: 1.0, delay: 10, options: [], animations: {self.decode()}, completion: nil)
+//     UIView.animate(withDuration: 1.0, delay: 15.5, options: [], animations: {self.dataFetch()}, completion: nil)
+//        UIView.animate(withDuration: 1.0, delay: 17.5, options: [], animations: {self.iExecution()}, completion: nil)
+// UIView.animate(withDuration: 1.0, delay: 20.5, options: [], animations: {self.rresult()}, completion: nil)
 //        fetch()
 //
 //        decode()
